ad1080184c014787b73b9f1e96fd70b88072c589
Paranoia8972 <contact@encryptopia.dev>
2024-05-04 13:38:10 +0200

    Added RSS Feed

---
+++ src/app/feed.xml/route.ts
@@ -0,0 +1,43 @@
+import RSS from 'rss';
+import { posts } from "#site/content";
+import { siteConfig, SiteConfig } from '#config';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    alternates: {
+        types: {
+            'application/rss+xml': siteConfig.url + '/feed.xml',
+        },
+    }
+};
+
+export async function GET() {
+    const feed = new RSS({
+        title: siteConfig.name,
+        description: siteConfig.description,
+        site_url: siteConfig.url,
+        feed_url: siteConfig.url + '/feed.xml',
+        copyright: `${new Date().getFullYear()} ${siteConfig.name}`,
+        language: 'en',
+        pubDate: new Date(),
+    });
+    posts.filter(post => post.published).map((post) => {
+        feed.item({
+            title: post.title,
+            guid: post.slug,
+            url: post.slug,
+            date: post.date,
+            description: post.description,
+            author: siteConfig.author,
+            categories: post.tags,
+        });
+    });
+
+    return new Response(feed.xml({ indent: true }), {
+        headers: {
+            'Content-Type': 'application/atom+xml; charset=utf-8',
+        },
+    });
+}
