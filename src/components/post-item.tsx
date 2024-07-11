ea31d8f9bd4ed5f9fce3c74edb5d61ff05033956
Paranoia8972 <contact@encryptopia.dev>
2024-07-11 11:33:29 +0200

    Added Loading State

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { cn, formatDate } from "@/lib/utils";
 import { Tag } from "./tag";
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface PostItemProps {
   slug: string;
@@ -10,9 +11,34 @@ interface PostItemProps {
   date: string;
   tags?: Array<string>;
   img: string;
+  loading?: boolean;
 }
 
-export function PostItem({ slug, title, date, tags, img }: PostItemProps) {
+export function PostItem({
+  slug,
+  title,
+  date,
+  tags,
+  img,
+  loading,
+}: PostItemProps) {
+  if (loading) {
+    return (
+      <div className="m-auto flex flex-col">
+        <Skeleton className="aspect-video h-[200px] w-[400px] rounded-xl border" />
+        <div className="px-4">
+          <div className="mt-2 flex flex-wrap items-center justify-between">
+            <Skeleton className="h-8 w-[150px]" />
+            <div className="flex h-[22px] flex-wrap gap-2">
+              <Skeleton className={`h-full w-[50px]`} />
+              <Skeleton className={`h-full w-[80px]`} />
+            </div>
+          </div>
+          <Skeleton className="mt-1 h-4 w-1/4" />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="m-auto flex flex-col">
       <Link href={"/" + slug}>
