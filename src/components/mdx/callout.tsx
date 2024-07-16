3f370bdf33fd2441a393d27db1acb43ca91c89a2
Paranoia8972 <contact@encryptopia.dev>
2024-07-16 20:11:45 +0200

    FIxed Callout resizing when toggling them

--- src/components/mdx/callout.tsx
+++ src/components/mdx/callout.tsx
@@ -15,11 +15,11 @@ interface CalloutProps {
 }
 
 const calloutIcon = {
-  info: <CircleAlert className="mr-2 h-6 w-6" />,
-  danger: <Flame className="mr-2 h-6 w-6" />,
-  caution: <TriangleAlert className="mr-2 h-6 w-6" />,
+  info: <CircleAlert className="mr-2 size-6" />,
+  danger: <Flame className="mr-2 size-6" />,
+  caution: <TriangleAlert className="mr-2 size-6" />,
   tip: <Lightbulb className="mr-2 h-6 w-6" />,
-  note: <Info className="mr-2 h-6 w-6" />,
+  note: <Info className="mr-2 size-6" />,
 };
 
 export function Callout({
@@ -31,17 +31,17 @@ export function Callout({
   return (
     <div
       className={cn(
-        "not-prose mb-6 rounded-md border border-l-4 border-l-current p-4 text-sm dark:border-b-0 dark:border-r-0 dark:border-t-0",
+        "not-prose mb-6 rounded-md border border-l-4 border-l-current p-4 text-sm dark:border-b-transparent dark:border-r-transparent dark:border-t-transparent",
         {
-          "bg-red-50 text-red-700 dark:prose dark:bg-red-600/10 dark:text-red-400":
+          "bg-red-50 text-red-700 dark:bg-red-600/10 dark:text-red-400":
             type === "danger",
-          "bg-yellow-50 text-yellow-700 dark:prose dark:bg-yellow-600/10 dark:text-yellow-400":
+          "bg-yellow-50 text-yellow-700 dark:bg-yellow-600/10 dark:text-yellow-400":
             type === "caution",
-          "bg-blue-50 text-blue-700 dark:prose dark:bg-blue-600/10 dark:text-blue-400":
+          "bg-blue-50 text-blue-700 dark:bg-blue-600/10 dark:text-blue-400":
             type === "info",
-          "bg-green-50 text-green-700 dark:prose dark:bg-green-600/10 dark:text-green-400":
+          "bg-green-50 text-green-700 dark:bg-green-600/10 dark:text-green-400":
             type === "tip",
-          "bg-gray-50 text-gray-700 dark:prose dark:bg-gray-600/10 dark:text-gray-400":
+          "bg-gray-50 text-gray-700 dark:bg-gray-600/10 dark:text-gray-400":
             type === "note",
         },
       )}
