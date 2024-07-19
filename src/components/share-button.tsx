97d0551aca2e8c857f8952cfa820c58672b935f5
Paranoia8972 <contact@encryptopia.dev>
2024-07-19 17:36:08 +0200

    Updated icons

--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import React from "react";
-import { FaXTwitter, FaLinkedin } from "react-icons/fa6";
-import { FaRedditAlien } from "react-icons/fa";
-import { IoLink } from "react-icons/io5";
+import { PiRedditLogoBold } from "react-icons/pi";
+
+import { Linkedin, Twitter } from "lucide-react";
 
 interface ShareButtonProps {
   text: string;
@@ -11,18 +11,18 @@ interface ShareButtonProps {
 
 const ShareButton: React.FC<ShareButtonProps> = ({ text, url }) => {
   const twitterShareUrl = `https://x.com/intent/tweet?text=${encodeURIComponent(
-    text
+    text,
   )}&url=${encodeURIComponent(url)}`;
   const redditShareUrl = `https://www.reddit.com/submit?url=${encodeURIComponent(
-    url
+    url,
   )}&title=${encodeURIComponent(text)}`;
   const linkedinShareUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
-    url
+    url,
   )}&title=${encodeURIComponent(text)}`;
 
   return (
-    <div className="flex flex-col space-y-2 ml-auto">
-      <p className="text-gray-400 text-sm">Share this post</p>
+    <div className="ml-auto flex flex-col space-y-2">
+      <p className="text-sm text-gray-400">Share this post</p>
       <div className="flex space-x-4">
         <Link
           href={twitterShareUrl}
@@ -30,7 +30,7 @@ const ShareButton: React.FC<ShareButtonProps> = ({ text, url }) => {
           rel="noopener noreferrer"
           className=""
         >
-          <FaXTwitter className="size-5 text-slate-600 hover:text-slate-600/80" />
+          <Twitter className="size-5 text-slate-600 hover:text-slate-600/80" />
         </Link>
         <Link
           href={redditShareUrl}
@@ -38,7 +38,7 @@ const ShareButton: React.FC<ShareButtonProps> = ({ text, url }) => {
           rel="noopener noreferrer"
           className="pl-1"
         >
-          <FaRedditAlien className="size-5 text-slate-600 hover:text-slate-600/80" />
+          <PiRedditLogoBold className="size-5 text-slate-600 hover:text-slate-600/80" />
         </Link>
         <Link
           href={linkedinShareUrl}
@@ -46,7 +46,7 @@ const ShareButton: React.FC<ShareButtonProps> = ({ text, url }) => {
           rel="noopener noreferrer"
           className="pl-1"
         >
-          <FaLinkedin className="size-5 text-slate-600 hover:text-slate-600/80" />
+          <Linkedin className="size-5 text-slate-600 hover:text-slate-600/80" />
         </Link>
         {/* <Link href={""} target="_blank" rel="noopener noreferrer" className="pl-1">
                     <IoLink className="size-5 text-slate-600 hover:text-slate-600/80" />
